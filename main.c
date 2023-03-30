2aee22628fd888b3e02479ad5ece9f8e34e099c2
sarahmohamedsalah <72987289+sarahmohamedsalah@users.noreply.github.com>
2023-03-30 12:57:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,53 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "LCS.h"
- uint8_t size;
+
 
 
 int main(void)
 {
-    uint8_t val;
-    int8_t ret;
-    int8_t i;
-    printf("Please enter array size: ");
-    scanf("%c", &size);
-    int32_t array[size];
-
-    for (i = 0; i < size ; i++)
-    {   printf("Please enter array element ");
-        scanf("%d", array+i);
-    }
-
-    uint8_t arrSize = sizeof(array) / sizeof(array[0]);
-
-   (void)insertionSort(array, arrSize);
-
-    printArray(array, arrSize);
-
-
-    ret = lcsGetSize(array, arrSize, &val);
-
-    if(ret == -1)
-    {
-        printf("\nEMPTY_ARRAY\n");
-    }
-    else if( ret == -2)
-    {
-        printf("\nARRAY_SIZE error \n");
-    }
-    else if( ret == -3 )
-    {
-        printf("\nNO_LCS \n");
-    }
-    else
-    {
-        printf(" \n %d ", val);
-        // printArray(array, arrSize);
-    }
-
-
-
-
-
-    return 0;
+    int32_t array[10]= {2, 8, 4, 10, 6, 20, 16, 12, 14, 16};
+    int8_t flag;
+    uint8_t LCS=0;
+    printf("array before sorting\n");
+    printArray(array,10);
+    flag=lcsGetSize(array,10,&LCS);
+    if(flag==-1)
+        printf("the array is empty");
+    if(flag==-2)
+        printf("the array is out of range\n");
+    printf("array after sorting\n");
+    printArray(array,10);
+    printf("LCS=%d",LCS);
 }
