013686da57e1429262b42e9387f3f9206556ec63
sarahmohamedsalah <72987289+sarahmohamedsalah@users.noreply.github.com>
2023-03-30 12:56:48 +0200

    Update lcs.c

--- lcs.c
+++ lcs.c
@@ -1,96 +1,63 @@
 #include "LCS.h"
 
-int8_t insertionSort(int32_t *array, uint8_t arraySize){
-int i, element, j;
-int8_t returnValue = 0;
-if(arraySize > 0 && arraySize < 11){
-        for (i = 1; i < arraySize; i++) {
-         element = array[i];
-j = i - 1; /* Move elements of arr[0..i-1], that are greater than key by one position */
- while (j >= 0 && array[j] > element) {
-            array[j + 1] = array[j];
-            j = j - 1;
+int8_t insertionSort(int32_t *array, uint8_t arraySize)
+{
+    uint8_t key,x,y;
+    int32_t temp;
+    if(arraySize == 0)
+        return -1;  //the array is empty
+    if(arraySize < 0 || arraySize >10)
+        return -2;  //if the array size is 0 or >10
+    for(x=1; x<arraySize; x++)
+    {
+        y=x;
+        while(array[y] < array[y-1] && y > 0)
+        {
+            temp=array[y];
+            array[y]=array[y-1];
+            array[y-1]=temp;
+            y--;
         }
-        array[j + 1] = element;
     }
-    returnValue = 0;
+    return 0;       //sorting is done without errors
 }
 
-   else if(arraySize ==0 ){
-
-        returnValue = -1;}
-
-else if( arraySize >10){
-    returnValue = -2;
-
-
-
-
-}return	returnValue;
+void printArray(int32_t *array, uint8_t arraySize)
+{int x;
+    for( x=0 ; x<arraySize; x++)
+    {
+        printf("%d ",array[x]);
+    }
+    printf("\n");
 }
 
-
-
-void printArray(int32_t *array, uint8_t arraySize){
-uint8_t i;
-    for (i = 0; i < arraySize; i++){}
-        printf("%d ", array[i]);
-   printf("\n\n");
-
-}
-
-int8_t lcsGetSize(int32_t *array, uint8_t arraySize, uint8_t *sizeofLCS){
-uint8_t counter = 1, i = 0;
-	int8_t functionReturn = 0, returnValue = 0;
-
-	*sizeofLCS = 1;
-
-	functionReturn = insertionSort(array, arraySize);
-
-	if(! functionReturn)
-	{
-		while (i < arraySize)
-		{
-			if (array[i + 1] - array[i] == 1)
-			{
-				counter++;
-				if (counter > *sizeofLCS)
-				{
-					*sizeofLCS = counter;
-				}
-				else
-				{
-					/*DO NOTHING*/
-				}
-			}
-			else
-			{
-				counter = 1;
-			}
-			i++;
-		}
-		if (*sizeofLCS > 1)
-		{
-			returnValue = 0;
-		}
-		else
-		{
-			returnValue = -3;
-		}
-	}
-	else if (functionReturn == -1)
-	{
-		returnValue = -1;
-	}
-	else if (functionReturn == -2)
-	{
-		returnValue = -2;
-	}
-	else
-	{
-		/*DO NOTHING*/
-	}
-	return returnValue;
+int8_t lcsGetSize(int32_t *array, uint8_t arraySize, uint8_t *sizeofLCS)
+{
+    uint8_t count=0,max=0,j=0,return_value;
+    return_value=insertionSort(array,arraySize);
+    if(return_value==0)
+    {int i;
+        for( i=0; i<arraySize; i++)
+        {
+            j=i+1;
+            if(array[i]==array[j]-1)
+                count++;
+            else
+            {
+                if(max < count)
+                {
+                    max=count+1;
+                    count=0;
+                }
+            }
+        }
+        *sizeofLCS=max;
+        return 0;
+    }
+    else if(return_value==-1)
+        return -1;
+    else
+        return -2;
 
 }
 
